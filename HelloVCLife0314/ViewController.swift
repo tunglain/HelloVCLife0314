7b84456da24f117df736cbd2266c9c76e06cabf4
Steven Hsu <tunglain@gmail.com>
2020-03-14 11:35:01 +0800

    Initial Commit

---
+++ HelloVCLife0314/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloVCLife0314
+//
+//  Created by 棟樑許 on 2020/3/14.
+//  Copyright © 2020 棟樑許. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
